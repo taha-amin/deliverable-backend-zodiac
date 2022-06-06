ea5e760ea5d7e49df5f559a0301e7816aa7a7cd1
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-06-06 16:24:19 -0700

    wrote test for first function

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,9 +3,19 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+const { zodiacs } = require('../data/zodiacs');
+
 describe('deliverable-backend-zodiac routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+
+  it('/zodiacs should return a list of zodiacs', async () => {
+    const res = await request(app).get('/zodiacs');
+    const expected = zodiacs.map((zodiac) => {
+      return { id: zodiac.id, name: zodiac.name };
+    });
+    expect(res.body).toEqual(expected);
   });
 
   afterAll(() => {
