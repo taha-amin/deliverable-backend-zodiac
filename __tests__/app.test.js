af2cdc1ce2da623625770af65f45146d5f28060a
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-06-06 16:54:03 -0700

    wrote second test for returning zodiac detail in app.test.js

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -18,6 +18,17 @@ describe('deliverable-backend-zodiac routes', () => {
     expect(res.body).toEqual(expected);
   });
 
+  it('/zodiacs/:id should return zodiac detail', async () => {
+    const res = await request(app).get('/zodiacs/1');
+    const aquarius = {
+      id: '1',
+      name: 'aquarius',
+      dates: 'Jan 21 - Feb 19',
+      symbol: 'Water Bearer',
+    };
+    expect(res.body).toEqual(aquarius);
+  });
+
   afterAll(() => {
     pool.end();
   });
